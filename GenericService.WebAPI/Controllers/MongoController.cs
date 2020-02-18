2a7a72ccef9c08428bb5adadb85508a66cc7b01e
Mr. Transistor <hexmustdie@gmail.com>
2020-02-18 23:38:44 +0300

    Change return type for Get methods of MongoController

--- GenericService.WebAPI/Controllers/MongoController.cs
+++ GenericService.WebAPI/Controllers/MongoController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GenericService.DAL.Services.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Bson.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace GenericService.WebAPI.Controllers
 {
@@ -20,27 +23,22 @@ namespace GenericService.WebAPI.Controllers
 
         // GET: api/Mongo
         [HttpGet]
-        public ContentResult Get()
+        public JsonResult Get()
         {
             var result = unitOfWork.Docs.Get().ToJson(writerSettings);
-            return Content(result);
+            return new JsonResult(JArray.Parse(result));
         }
-
-        public override ContentResult Content(string result)
-        {
-            return Content(result, "application/json");
-        }
-
+        
         // GET: api/Mongo/5e3d20279aa6ca09b88e95c4
         [HttpGet("{id}", Name = "Get")]
-        public ContentResult Get(string id)
+        public JsonResult Get(string id)
         {
-            return Content(unitOfWork.Docs.FindById(id).ToJson(writerSettings));
+            return new JsonResult(JObject.Parse(unitOfWork.Docs.FindById(id).ToJson(writerSettings)));
         }
 
         // POST: api/Mongo
         [HttpPost]
-        public IActionResult Post([FromBody] object value)
+        public IActionResult Post([FromBody] JObject value)
         {
             if (ModelState.IsValid && value != null)
             {
@@ -52,8 +50,14 @@ namespace GenericService.WebAPI.Controllers
 
         // PUT: api/Mongo/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(string id, [FromBody] JObject value)
         {
+            if (ModelState.IsValid && !string.IsNullOrEmpty(id))
+            {
+                //unitOfWork.Docs.Update(unitOfWork.Docs.FindById(id), BsonDocument.(value));
+                return Ok(value);
+            }
+            return BadRequest(ModelState);
         }
 
         // DELETE: api/ApiWithActions/5
