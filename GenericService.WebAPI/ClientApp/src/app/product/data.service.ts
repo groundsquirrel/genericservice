81ca156f5c9b8c63800029a6fbd17c89e4c05e33
Mr. Transistor <hexmustdie@gmail.com>
2020-02-21 00:05:49 +0300

    Added product view.

---
+++ GenericService.WebAPI/ClientApp/src/app/product/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Product } from './product';
+ 
+@Injectable()
+export class DataService {
+ 
+    private url = "/api/generic";
+ 
+    constructor(private http: HttpClient) {
+    }
+ 
+    getProducts() {
+        return this.http.get(this.url);
+    }
+ 
+    createProduct(product: Product) {
+        return this.http.post(this.url, product);
+    }
+    updateProduct(product: Product) {
+  
+        return this.http.put(this.url + '/' + product.id, product);
+    }
+    deleteProduct(id: number) {
+        return this.http.delete(this.url + '/' + id);
+    }
+}
